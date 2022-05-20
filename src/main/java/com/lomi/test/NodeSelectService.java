0630bb832cd04dc01e45e21601c4243f58bdfccf
张玉坤 <451740146@qq.com>
2022-05-20 22:00:21 +0800

    使用idea的coverage，测试行覆盖率85%以上的代码

---
+++ src/main/java/com/lomi/test/NodeSelectService.java
@@ -0,0 +1,22 @@
+package com.lomi.test;
+
+public class NodeSelectService {
+
+    public void  select( int i ){
+        if( i>0 ){
+            System.out.printf("大于0");
+        }else if(i<0){
+            System.out.printf("小于于0");
+        }else{
+            System.out.printf("等于0");
+
+
+
+        }
+
+        System.out.printf("");
+    }
+
+
+
+}
