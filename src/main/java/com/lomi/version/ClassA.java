7672be8df17c5fd137062eefd9bb57be1e4c98ee
张玉坤 <451740146@qq.com>
2022-05-21 12:50:16 +0800

    在dev 上提交 ClassA

---
+++ src/main/java/com/lomi/version/ClassA.java
@@ -0,0 +1,16 @@
+package com.lomi.version;
+
+public class ClassA {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
