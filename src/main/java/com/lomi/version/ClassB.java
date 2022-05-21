be239b688987ee567b7b2655ce0190006b8602c1
张玉坤 <451740146@qq.com>
2022-05-21 12:57:04 +0800

    在main 上提交 classB

---
+++ src/main/java/com/lomi/version/ClassB.java
@@ -0,0 +1,19 @@
+package com.lomi.version;
+
+public class ClassB {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    private int stock;
+
+
+
+}
