73b9ae8ee0be18ad5419ade3c3eec8c33775ee0b
张玉坤 <451740146@qq.com>
2022-05-23 23:44:14 +0800

    自定义定时任务

--- src/main/java/com/lomi/config/scheduling/MyScheduling1.java
+++ src/main/java/com/lomi/config/scheduling/MyScheduling1.java
@@ -4,12 +4,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import cn.hutool.core.date.LocalDateTimeUtil;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.scheduling.support.PeriodicTrigger;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -20,14 +21,23 @@ import org.springframework.util.CollectionUtils;
 //@Component
 public class MyScheduling1 implements SchedulingConfigurer {
 
+    ExecutorService ex = new ThreadPoolExecutor(1,
+            10,30,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue(100) );
+
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar registrar) {
 
-        Runnable r = ()->{
-            System.out.println( LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))  ) ;
+        Runnable task = ()->{
+            ex.execute( ()->System.out.println( LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))  )  );
         };
+
         PeriodicTrigger periodicTrigger = new PeriodicTrigger(5,TimeUnit.SECONDS);
-        registrar.addTriggerTask(r,periodicTrigger);
+        registrar.addTriggerTask(task, a -> {
+                    return new CronTrigger("").nextExecutionTime(a);
+                });
 
     }
 }
